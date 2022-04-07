6c359eb6b5a1178513ec1ef630d4009e7e2c3100
mono0218 <mona.hiro@monaka.org>
2022-04-07 13:39:05 +0900

    config edit

--- index.js
+++ index.js
@@ -7,23 +7,23 @@ client.on("ready", () => {
 
 client.on("message", async message => {
   if (message.author.bot) return;
-  const messages = await message.guild.channels.cache.get('961206059199000579')
+  const messages = await message.guild.channels.cache.get(config.channelid1)
 
   const embed = new Discord.MessageEmbed()
       .setTitle("" + '匿名システム')
       .addField("送信した内容:", message.content)
-  client.channels.cache.get('961457265574047784').send(embed)
+  client.channels.cache.get(config.channelid1).send(embed)
   
 })
 
 client.on("message", async message => {
   if (message.author.bot) return;
-  const messages = await message.guild.channels.cache.get('961457265574047784')
+  const messages = await message.guild.channels.cache.get(config.channelid1)
 
   const embed1 = new Discord.MessageEmbed()
       .setTitle("ユーザー: " + message.author.tag)
       .addField("送信した内容:", message.content)
-  client.channels.cache.get('961206059199000579').send(embed1)
+  client.channels.cache.get(config.channelid1).send(embed1)
   
 })
 
