82c34e1ec4a64acd317b876fc82a022557b37ab4
mono0218 <mona.hiro@monaka.org>
2022-04-07 14:11:41 +0900

    edit

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
 const Discord = require("discord.js");
+const {channelid1, channelid2, token,} = require("./config.json");
 const client = new Discord.Client();
-const config = require("./config.json");
 
 client.on("ready", () => {
   console.log(`起動しました`);
 });
 
 client.on("message", async message => {
-  if (message.author.bot) return;
-  const messages = await message.guild.channels.cache.get(config.channelid1)
 
+  if (message.author.bot) return;
+  if (message.channel.id === channelid1){
   const embed = new Discord.MessageEmbed()
       .setTitle("" + '匿名システム')
       .addField("送信した内容:", message.content)
-  client.channels.cache.get(config.channelid1).send(embed)
-  
+  client.channels.cache.get(channelid2).send(embed)
+  }
+
 })
 
 client.on("message", async message => {
   if (message.author.bot) return;
-  const messages = await message.guild.channels.cache.get(config.channelid1)
+  if (message.channel.id === channelid2){
 
   const embed1 = new Discord.MessageEmbed()
       .setTitle("ユーザー: " + message.author.tag)
       .addField("送信した内容:", message.content)
-  client.channels.cache.get(config.channelid1).send(embed1)
-  
+  client.channels.cache.get(channelid1).send(embed1)
+  }
 })
 
 
-client.login(config.token);
+client.login(token);
 
 //created by mono0218
